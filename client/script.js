3f9faf57c9466a86f99f00b936317f693ac085c4
ketanprakash <ketanprakash001@gmail.com>
2022-09-19 20:48:59 +0530

    ok

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const socket = io('https://drab-ruby-buffalo-toga.cyclic.app/', {
+const socket = io('https://socket-chat-wds.onrender.com/', {
   transports: ["websocket"]
 });
 
